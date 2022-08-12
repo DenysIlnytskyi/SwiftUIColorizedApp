54b72d38d88a7ad9376f880a43b294078ac0d7cc
DenysIlnytskyi <380984511271k@gmail.com>
2022-08-12 17:14:14 +0200

    Initial Commit

---
+++ SwiftUIColorizedApp/SwiftUIColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIColorizedAppApp.swift
+//  SwiftUIColorizedApp
+//
+//  Created by Денис Ільницький on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
